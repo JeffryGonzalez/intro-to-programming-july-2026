b2b2975f5dfe4935a91fc3ba498ef7e0c5cdc8d5
Jeff Gonzalez <jeff@hypertheory.com>
2026-07-09 13:18:56 -0400

    JeffDemo

---
+++ practices/StringCalculatorKataThursdayJeffDemo/StringCalculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+
+        return numbers == "" ? 0 :
+             numbers.Split(',', '\n')
+             .Sum(int.Parse);
+
+    }
+}
---
+++ practices/StringCalculatorKataThursdayJeffDemo/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,63 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    private Calculator calculator;
+    public CalculatorTests()
+    {
+        calculator = new Calculator();
+    }
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("12", 12)]
+    [InlineData("108", 108)]
+    public void SingleDigits(string nums, int expected)
+    {
+        var answer = calculator.Add(nums);
+
+        Assert.Equal(expected, answer);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("2,2", 4)]
+    [InlineData("12,3", 15)]
+    [InlineData("108,300", 408)]
+
+    public void TwoNumbers(string nums, int expected)
+    {
+        var answer = calculator.Add(nums);
+
+        Assert.Equal(expected, answer);
+    }
+
+    [Theory]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    [InlineData("120, 10, 10", 140)]
+    public void Arbitrary(string nums, int expected)
+    {
+        var answer = calculator.Add(nums);
+
+        Assert.Equal(expected, answer);
+    }
+
+    [Theory]
+    [InlineData("1\n2", 3)]
+    [InlineData("1\n5", 6)]
+    [InlineData("1,2\n3", 6)]
+    public void MixedDelimeters(string nums, int expected)
+    {
+        var answer = calculator.Add(nums);
+
+        Assert.Equal(expected, answer);
+    }
+}
